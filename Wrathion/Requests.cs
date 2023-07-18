ed7c691d2982bfe90b9c0ab3fe932be2ac298266
Coaixy <coaixy@outlook.com>
2023-07-18 11:40:16 +0800

    Change:Add new method and change old method:GetCategory

--- Wrathion/Requests.cs
+++ Wrathion/Requests.cs
@@ -125,19 +125,10 @@ namespace Wrathion
             return obj["data"]?[0]?["userProjectId"]?.ToString();
         }
 
-        public static List<string> GetCategory()
+        public static List<string> GetFinishIdList()
         {
+            var obj = GetCategory();
             var r = new List<string>();
-            var url = "https://weiban.mycourse.cn/pharos/usercourse/listCategory.do";
-            var data = new Dictionary<string, string>()
-            {
-                { "userId", GetValue("userId") },
-                { "tenantCode", GetValue("tenantCode") },
-                { "chooseType", "3" },
-                { "userProjectId", GetValue("userProjectId") }
-            };
-            var text = Post(url, data);
-            var obj = JObject.Parse(text)["data"];
             foreach (var item in obj)
             {
                 var totalNum =  Convert.ToInt32(item["totalNum"]);
@@ -149,6 +140,20 @@ namespace Wrathion
                 }
             }
             return r;
+        }
+        public static JToken GetCategory()
+        {
+            var url = "https://weiban.mycourse.cn/pharos/usercourse/listCategory.do";
+            var data = new Dictionary<string, string>()
+            {
+                { "userId", GetValue("userId") },
+                { "tenantCode", GetValue("tenantCode") },
+                { "chooseType", "3" },
+                { "userProjectId", GetValue("userProjectId") }
+            };
+            var text = Post(url, data);
+            var obj = JObject.Parse(text)["data"];
+            return obj;
         }
         
         public static void test()
